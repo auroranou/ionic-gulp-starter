e6f733da0f12c1586bbd345d8fbe7875ee3e93b4
Aurora Nou <auroranou@gmail.com>
2015-12-14 17:24:57 -0500

    refactor ng-annotate task into larger build task to annotate, concatenate, minify, and uglify; add lint task to default and watch

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,9 @@ var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
 var ngAnnotate = require('gulp-ng-annotate');
+var lint = require('gulp-eslint');
+var minify = require('gulp-minify');
+var uglify = require('gulp-uglify');
 
 var paths = {
   sass: ['./scss/**/*.scss'],
@@ -14,7 +17,7 @@ var paths = {
   js: ['./www/js/**/*.js']
 };
 
-gulp.task('default', ['sass', 'annotate']);
+gulp.task('default', ['sass', 'lint']);
 
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
@@ -31,7 +34,7 @@ gulp.task('sass', function(done) {
 
 gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
-  gulp.watch(paths.js, ['annotate']);
+  gulp.watch(paths.js, ['lint']);
 });
 
 gulp.task('install', ['git-check'], function() {
@@ -54,9 +57,22 @@ gulp.task('git-check', function(done) {
   done();
 });
 
-gulp.task('annotate', function(done) {
+// --------- custom gulp tasks ---------
+
+gulp.task('lint', function() {
   gulp.src('./www/js/**/*.js')
-  .pipe(ngAnnotate({single_quotes: true}))
-  .pipe(gulp.dest('./www/dist/js'))
-  .on('end', done);
+    .pipe(lint())
+    .pipe(lint.format())
+    // if an error is found, exit the task
+    // .pipe(lint.failAfterError())
 });
+
+gulp.task('build', ['lint', 'sass'], function() {
+  gulp.src('./www/js/**/*.js')
+    .pipe(concat('app'))
+    .pipe(ngAnnotate({ single_quotes: true }))
+    .pipe(minify())
+    .pipe(uglify())
+    .pipe(rename({ extname: ".min.js" }))
+    .pipe(gulp.dest('./www/dist/js/'))
+})
