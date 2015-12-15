dfb98cc48a1eee16453381d27d7eaa14c84c42f8
Aurora Nou <auroranou@gmail.com>
2015-12-15 12:58:32 -0500

    refactor gulp tasks, point ionic.project documentRoot to src directory

--- gulpfile.js
+++ gulpfile.js
@@ -6,37 +6,98 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var del = require('del');
 var ngAnnotate = require('gulp-ng-annotate');
 var lint = require('gulp-eslint');
 var minify = require('gulp-minify');
 var uglify = require('gulp-uglify');
+var htmlreplace = require('gulp-html-replace');
+var replace = require('gulp-replace');
 
 var paths = {
   sass: ['./scss/**/*.scss'],
-  templates: ['./www/templates/**/*.html'],
-  js: ['./www/js/**/*.js']
+  js: ['./src/js/**/*.js'],
+  templates: ['./src/templates/**/*.html'],
+  lib: ['./src/lib/**/*'],
+  img: ['./src/img/**/*'],
+  index: ['./src/index.html']
 };
+
+// === top level tasks ===
 
 gulp.task('default', ['sass', 'lint']);
 
-gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .on('error', sass.logError)
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
-});
+gulp.task('build', ['clean', 'compile-js', 'compile-html', 'compile-css']);
 
 gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
   gulp.watch(paths.js, ['lint']);
 });
 
+// === utility tasks ===
+
+gulp.task('sass', function(done) {
+  gulp.src('./scss/ionic.app.scss')
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(gulp.dest('./src/css/'))
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./src/css/'))
+    .on('end', done);
+});
+
+gulp.task('lint', function() {
+  gulp.src('./src/js/**/*.js')
+    .pipe(lint())
+    .pipe(lint.format())
+    // if an error is found, exit the task
+    // .pipe(lint.failAfterError())
+});
+
+gulp.task('clean', function() {
+  del('./www/**/*');
+});
+
+// === child tasks ===
+
+gulp.task('compile-js', function() {
+  gulp.src(paths.js)
+    .pipe(concat('app'))
+    .pipe(ngAnnotate({ single_quotes: true }))
+    .pipe(minify())
+    .pipe(uglify())
+    .pipe(rename({ extname: ".min.js" }))
+    .pipe(gulp.dest('./www/js'));
+
+  gulp.src(paths.lib)
+    .pipe(gulp.dest('./www/lib'));
+});
+
+gulp.task('compile-html', function() {
+  gulp.src(paths.index)
+    .pipe(htmlreplace({
+      'css': 'css/ionic.app.min.css',
+      'js': 'js/app.min.js',
+    }))
+    .pipe(replace(/<body /, '<body ng-strict-di '))
+    .pipe(gulp.dest('./www'));
+
+  gulp.src(paths.templates)
+    .pipe(gulp.dest('./www/templates'));
+
+  gulp.src(paths.img)
+    .pipe(gulp.dest('./www/img'));
+});
+
+gulp.task('compile-css', ['sass'], function() {
+  gulp.src('./src/css/ionic.app.min.css')
+    .pipe(gulp.dest('./www/css'));
+});
+
+// === other ionic tasks ===
 gulp.task('install', ['git-check'], function() {
   return bower.commands.install()
     .on('log', function(data) {
@@ -56,23 +117,3 @@ gulp.task('git-check', function(done) {
   }
   done();
 });
-
-// --------- custom gulp tasks ---------
-
-gulp.task('lint', function() {
-  gulp.src('./www/js/**/*.js')
-    .pipe(lint())
-    .pipe(lint.format())
-    // if an error is found, exit the task
-    // .pipe(lint.failAfterError())
-});
-
-gulp.task('build', ['lint', 'sass'], function() {
-  gulp.src('./www/js/**/*.js')
-    .pipe(concat('app'))
-    .pipe(ngAnnotate({ single_quotes: true }))
-    .pipe(minify())
-    .pipe(uglify())
-    .pipe(rename({ extname: ".min.js" }))
-    .pipe(gulp.dest('./www/dist/js/'))
-})
